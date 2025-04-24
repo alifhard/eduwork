cc126cc4d3c9be323e2c3e2b9a242381768e0cfe
Alif Harbi <124515041+alifhard@users.noreply.github.com>
2025-04-24 22:34:59 +0700

    Add files via upload
    pemanggilan module

---
+++ tugas module/index.js
@@ -0,0 +1,4 @@
+import getOrang from './module1.js';
+
+const orang = getOrang();
+console.log(orang);
---
+++ tugas module/module1.js
@@ -0,0 +1,31 @@
+const getOrang = () => {
+    return [
+        {
+            nama: "zaki",
+            umur: 21,
+            pekerjaan: "operator"
+        },
+        {
+            nama: "alin",
+            umur: 22,
+            pekerjaan: "kasir"
+        },
+        {
+            nama: "budi",
+            umur: 23,
+            pekerjaan: "manager"
+        },
+        {
+            nama: "sari",
+            umur: 24,
+            pekerjaan: "staff"
+        },
+        {
+            nama: "andi",
+            umur: 25,
+            pekerjaan: "admin"
+        }
+    ];
+};
+
+export default getOrang;
